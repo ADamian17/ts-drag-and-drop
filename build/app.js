86008181036600587c77da087b70c4a06c0d3001
ADamian17 <adonisdesuvikdo@gmail.com>
2021-10-15 09:06:06 -0700

    added new class
    Signed-off-by: ADamian17 <adonisdesuvikdo@gmail.com>

--- build/app.js
+++ build/app.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log('hello world');
+const projectTemplate = document.getElementById('single-project');
+console.log(projectTemplate);
+const projectListTemplate = document.getElementById('project-list');
+console.log(projectListTemplate);
+class ClientForm {
+    constructor() {
+        this.handleSubmit = (e) => {
+            e.preventDefault();
+            console.log(this.titleInputElement.value);
+        };
+        this.templateElement = this.findElById('project-input');
+        this.hostElement = this.findElById('app');
+        this.element = this.getImportedNode(this.templateElement).firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
+        this.renderForm();
+    }
+    findElById(elementId) {
+        return document.getElementById(elementId);
+    }
+    getImportedNode(node) {
+        return document.importNode(node.content, true);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.handleSubmit);
+    }
+    renderForm() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const App = new ClientForm();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,52 @@
-console.log('hello world');
+/* dom vars */
+const projectTemplate = document.getElementById('single-project') as HTMLTemplateElement;
+console.log(projectTemplate);
+const projectListTemplate = document.getElementById('project-list') as HTMLTemplateElement;
+console.log(projectListTemplate);
+class ClientForm {
+
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = this.findElById('project-input')! as HTMLTemplateElement;
+    this.hostElement = this.findElById('app')! as HTMLDivElement;
+
+    this.element = this.getImportedNode(this.templateElement).firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+    this.configure()
+    this.renderForm()
+  }
+
+  private findElById(elementId: string) {
+    return document.getElementById(elementId)
+  }
+
+  private getImportedNode(node: any) {
+    return document.importNode(node.content, true);
+  }
+
+  private handleSubmit = (e: Event) => {
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.handleSubmit);
+  }
+
+  private renderForm() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const App = new ClientForm()
